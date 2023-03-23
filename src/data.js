0846f203009968ae3eb030d2cac3d06e37b5fb4b
RbkHeredia <rbkheredia@gmail.com>
2023-03-23 20:06:37 -0300

    fixed images

--- src/data.js
+++ src/data.js
@@ -2,25 +2,25 @@ var data = [
   {
     "category": "Reaction",
     "score": 80,
-    "icon": "/images/icon-reaction.svg",
+    "icon": "./images/icon-reaction.svg",
     "color": "rojo"
   },
   {
     "category": "Memory",
     "score": 92,
-    "icon": "/images/icon-memory.svg",
+    "icon": "./images/icon-memory.svg",
     "color": "amarillo"
   },
   {
     "category": "Verbal",
     "score": 61,
-    "icon": "/images/icon-verbal.svg",
+    "icon": "./images/icon-verbal.svg",
     "color": "verde"
   },
   {
     "category": "Visual",
     "score": 72,
-    "icon": "/images/icon-visual.svg",
+    "icon": "./images/icon-visual.svg",
     "color": "azul"
   }
 ]
