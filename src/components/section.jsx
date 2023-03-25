0729bd17bf305ae259598ac607ae354162b25cec
RbkHeredia <rbkheredia@gmail.com>
2023-03-25 10:52:47 -0300

    headings

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -4,7 +4,7 @@ export default function Section({ color, score, icon, category, alt }){
   return(
     <section className={color}>
       <img src={icon} alt={alt}/>
-      <h6>{category}</h6>
+      <h4>{category}</h4>
       <p className="opacity"><b>{score}</b></p>
       <p className="opacity">/ 100</p>
     </section>
